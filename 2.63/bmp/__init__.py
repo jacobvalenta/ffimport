67e0aa95f86ea42461934eb5ae3a69ce0aeac485
Jacob Valenta <jacob.valenta@gmail.com>
2012-08-25 19:30:31 -0400

    set up the BMP class
    sorry for the lack of commits over the last week.

--- 2.63/bmp/__init__.py
+++ 2.63/bmp/__init__.py
@@ -2,3 +2,19 @@ class bmp():
 	'''Easily creates bmp files given pixel data'''
 	def __init__(self):
 		pass
+
+	def data(self, pixels):
+		'''Sets the pixel data for the bitmap'''
+		pass
+
+	def save(self, filename):
+		'''Saves the bitmap the the filename provided (.bmp)'''
+		pass
+
+if __name__ == '__main__':
+	image = bmp()
+	image.data((
+				(0, 0, 0), (1, 0, 0), (1, 1, 0),
+				(0, 1, 1), (0, 0, 1), (1, 1, 1)
+				))
+	image.save('myPicture')
